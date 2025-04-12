d5abf560fa937aa4ab941a00a458ad03e49ece1b
GG-Muniz <g.garciamuniz@outlook.com>
2025-04-12 15:31:50 -0400

    Fixing compilation error on declaration of grandparent node and syntax error in a return statement.

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,20 +6,21 @@
  * binary_tree_uncle - funtion will find if the uncle of a node.
  * @node: pointer to the node that to find the uncle.
  * Return: NULL if no node and if no uncle.
- *
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *grandparent;
+	
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *grandparent = node->parent->parent;
+	grandparent = node->parent->parent;
 
 	if (grandparent->left == node->parent)
 		return (grandparent->right);
 
 	if (grandparent->right == node->parent)
-		return (gradparent->left);
+		return (grandparent->left);
 
 	return (NULL);
 }
